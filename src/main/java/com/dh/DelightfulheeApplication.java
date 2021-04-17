c34a1b7fa255d1a61d8aabeaab7396e7ee259169
heehee59 <sock42@daum.net>
2021-04-18 07:27:20 +0900

    🍕 first commit

---
+++ src/main/java/com/dh/DelightfulheeApplication.java
@@ -0,0 +1,13 @@
+package com.dh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelightfulheeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelightfulheeApplication.class, args);
+	}
+
+}
